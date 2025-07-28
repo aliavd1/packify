6845521057feaf8fa0215f855a7dd4a00eaf1596
Ali Vatandoost <alivatandoost95@gmail.com>
2025-07-28 23:11:17 +0330

    fix: remove fontawesome css file route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/style.css";
-import "./styles/fontawesome.min.css";
 
 const container = document.getElementById("root");
 
