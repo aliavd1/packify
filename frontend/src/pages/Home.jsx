b50175cb3b3bf7e2eac404d7cf98fbf1a9b4221d
Ali Vatandoost <alivatandoost95@gmail.com>
2025-07-28 23:24:34 +0330

    feat: use new stepper in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
 import ToggleThemeButton from "../components/ToggleThemeButton";
-import ArrowStepper from "../components/ArrowStepper";
+import Stepper from "../components/Stepper";
+import CompleteInfoForm from "../components/CompleteInfoForm";
+import { Moon, Sun } from "lucide-react";
 
 const Home = () => {
-  const steps = ["Complete info", "Select icon", "Select app", "Confirm"];
+  const steps = [
+    {
+      header: "Complete info",
+      content: <CompleteInfoForm />,
+    },
+    {
+      header: "Select icon",
+      content: <p>Select icon</p>,
+    },
+    {
+      header: "Select app",
+      content: <p>Select app</p>,
+    },
+    {
+      header: "Confirm",
+      content: <p>Confirm</p>,
+    },
+  ];
   const [isDark, setIsDark] = useState(() => {
     return JSON.parse(localStorage.getItem("isDark") || "false");
   });
@@ -28,15 +47,13 @@ const Home = () => {
         btnClasses="fixed bottom-5 right-5 w-12 h-12 bg-neutral-300 rounded-full shadow-lg 
           hover:bg-neutral-200 flex justify-center items-center dark:bg-neutral-700 
           dark:hover:bg-neutral-600 cursor-pointer active:scale-90 will-change-transform duration-300"
-        firstIcon="fa-sun-bright"
-        firstIconColor="black"
-        secondIcon="fa-moon"
-        secondIconColor="white"
+        FirstIcon={Sun}
+        SecondIcon={Moon}
         duration={300}
         onClick={() => setIsDark((value) => !value)}
       />
 
-      <ArrowStepper steps={steps} />
+      <Stepper steps={steps} />
     </div>
   );
 };
